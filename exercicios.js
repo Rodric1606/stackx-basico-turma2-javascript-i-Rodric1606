e1203794ed97e03d511fd5a916c17aeb393f17e7
Rodrigo dos Santos Cardozo <rodrigodsc.cardozo@gmail.com>
2024-02-04 16:00:53 -0300

    Exercicio concluído

--- exercicios.js
+++ exercicios.js
@@ -1,106 +1,119 @@
 // -------------------------------------
 
-/**
- * Função que calcula a soma de 2 números
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} A soma dos dois numeros
- */
+//Função que calcula a soma de 2 números
+  let a = 1337                      // O primeiro número.
+  let b = 3301                      // O segundo número.
+  let result = soma(a, b)       // A soma dos dois numeros
+ 
 function soma(a, b) {
-  // @todo implementar retorno da operação aqui
+  return  a + b;
 }
 
-/**
- * Função que calcula a multiplicação de 2 números
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} A multiplicação dos dois numeros
- */
+ // Função que calcula a multiplicação de 2 números
+  a = 3                             // O primeiro número.
+  b = 11                            // O segundo número.
+  result = multiplicacao(a,b) //A multiplicação dos dois numeros
+ 
 function multiplicacao(a, b) {
-  // @todo implementar retorno da operação aqui
+  return a * b;
 }
 
-/**
- * Função que calcula a divisão de 2 números
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} A divisão dos dois numeros
- */
+//Função que calcula a divisão de 2 números
+  a = 10                          // O primeiro número.
+  b = 2                           // O segundo número.
+  result = divisao(a,b)    // A divisão dos dois numeros
+
 function divisao(a, b) {
-  // @todo implementar retorno da operação aqui
+  return a / b;
 }
 
-/**
- * Função que calcula a subtração de 2 números
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} A subtração dos dois numeros
- */
+//Função que calcula a subtração de 2 números
+  a = 3301                        // O primeiro número.
+  b = 1337                        // O segundo número.
+  result = subtracao(a,b)  // A subtração dos dois numeros
+
 function subtracao(a, b) {
-  // @todo implementar retorno da operação aqui
+  return a - b;
 }
 
-/**
- * Função que resto da divisão de 2 números
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} O resto da divisão dos dois numeros
- */
+//Função que resto da divisão de 2 números
+a = 15                            // O primeiro número.
+b = 4                             // O segundo número.
+result = modulo(a,b)  // O resto da divisão dos dois numeros
+
 function modulo(a, b) {
-  // @todo implementar retorno da operação aqui
+   if(b === 0){
+      return 0;
+   } else{
+     return a % b;
+   }
 }
 
-/**
- * Função que calcula o maior número entre 2
- * @param {number} a - O primeiro número.
- * @param {number} b - O segundo número.
- * @returns {number} O maior número
- */
+//Função que calcula o maior número entre 2
+a = 13                            // O primeiro número.
+b = 33                            // O segundo número.
+result = max(a,b)
+
 function max(a, b) {
-  // @todo implementar retorno da operação aqui
+   if(a > b){
+     return a;
+   }else{
+     return b;
+   }
 }
-
 // -------------------------------------
+//Função que concatena 2 strings utilizando template strings
+  a = 'Hello'                      // A primeira string
+  b = 'World'                      // A segunda string
+  result = strConcat(a,b)
 
-/**
- * Função que concatena 2 strings utilizando template strings
- * @param {string} a - A primeira string
- * @param {string} b - A segunda string
- * @returns {string} As 2 strings concatenadas
- */
 function strConcat(a, b) {
-  // @todo implementar retorno da operação aqui
+    return `${a} ${b}`;
 }
 
 // --------------------------------------
+//Função que implementa controle de fluxo
 
-/**
- * Função que implementa controle de fluxo
- * 
- * @param {number} numero - O número em questão
- * @returns {string} 
- *  - "Maior que 10" quando o número é maior que 10
- *  - "Igual a 10" quando o número é igual a 10
- *  - "Menor que 10" quando o número é menor que 10
- */
+ let numero = 15                   // O número em questão
+ result = retornaNumero(numero)
+ numero = 10
+ result = retornaNumero(numero)
+ numero = 5 
+ result = retornaNumero(numero)
+
+/*"Maior que 10" quando o número é maior que 10
+  "Igual a 10" quando o número é igual a 10
+  "Menor que 10" quando o número é menor que 10 */
+
 function retornaNumero(numero) {
-  // @todo implementar retorno da operação aqui
+   if(numero == 10){
+     return `Igual a 10`;
+   }else if( numero > 10){
+      return `Maior que 10`;
+   }else{
+      return `Menor que 10`;
+   }
 }
-
 // --------------------------------------
+//Função que valida se o número em questão é positivo
+let num = 5                                                 // Número em questão
+result = ehPositivo(num)                                    // Retorna true para números positivos
+num = 0
+result = ehPositivo(num) 
+num = -5
+result = ehPositivo(num) 
 
-/**
- * Função que valida se o número em questão é positivo
- * @param {number} num - Número em questão
- * @returns {boolean} Retorna true para números positivos
- */
 function ehPositivo(num) {
-  // @todo implementar retorno da operação aqui
+  if(num >= 0){
+    return true
+  }else{
+    return false
+  }
 }
 
 module.exports = 
 { 
-  soma,
+ soma,
   multiplicacao,
   divisao,
   subtracao,
@@ -109,4 +122,4 @@ module.exports =
   strConcat,
   retornaNumero,
   ehPositivo
-};
+}; 
